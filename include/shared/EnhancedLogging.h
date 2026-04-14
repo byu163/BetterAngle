84b28935fbf5e038f7210ffc2a35b767f184f2b5
wavedropmaps <263283507+wavedropmaps@users.noreply.github.com>
2026-04-14 12:09:40 +1000

    v4.27.116: Build Recovery - Restored Logging System

--- include/shared/EnhancedLogging.h
+++ include/shared/EnhancedLogging.h
@@ -1,22 +1,66 @@
 #pragma once
 
 #include <string>
+#include <fstream>
+#include <mutex>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+#include <atomic>
+#include <memory>
+#include <windows.h>
+
+// Handle Windows ERROR macro collision
+#ifdef ERROR
+#undef ERROR
+#endif
 
 enum class LogLevel {
+    Trace,
     Debug,
     Info,
     Warning,
-    Error
+    Error,
+    Fatal
 };
 
+// Global level control
+extern std::atomic<LogLevel> g_logLevel;
+
+// Global functions
 void InitEnhancedLogging();
 void ShutdownEnhancedLogging();
 void SetLogLevel(LogLevel level);
 void LogStartup();
-void LogWindowInfo(const wchar_t* label, void* hwnd);
-void LogMessage(LogLevel level, const wchar_t* message);
+void LogWindowInfo(HWND hwnd);
 
-#define LOG_INFO(msg) LogMessage(LogLevel::Info, msg)
-#define LOG_WARNING(msg) LogMessage(LogLevel::Warning, msg)
-#define LOG_ERROR(msg) LogMessage(LogLevel::Error, msg)
-#define LOG_DEBUG(msg) LogMessage(LogLevel::Debug, msg)
+// Core logging functions
+void LogMessage(LogLevel level, const char* file, int line, const char* format, ...);
+
+// Macros
+#define LOG_TRACE(fmt, ...) LogMessage(LogLevel::Trace, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) LogMessage(LogLevel::Debug, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...)  LogMessage(LogLevel::Info,  __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...)  LogMessage(LogLevel::Warning, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) LogMessage(LogLevel::Error, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define LOG_FATAL(fmt, ...) LogMessage(LogLevel::Fatal, __FILE__, __LINE__, fmt, ##__VA_ARGS__)
+
+class EnhancedLogger {
+public:
+    static EnhancedLogger& Instance();
+
+    void Initialize(const std::wstring& logPath);
+    void Log(LogLevel level, const char* file, int line, const std::string& message);
+    void Flush();
+
+private:
+    EnhancedLogger() = default;
+    ~EnhancedLogger();
+
+    std::ofstream m_stream;
+    std::mutex m_mutex;
+    bool m_initialized = false;
+
+    std::string LevelToString(LogLevel level) const;
+    std::string TimestampNow() const;
+};
